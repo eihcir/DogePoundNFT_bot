a36d9dfb3da629d16e28c150e5aeda1a86cc6b38
Richie Humphrey <eihcir0@gmail.com>
2021-08-03 17:46:03 +0000

    update

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+import os
 import requests
 from datetime import datetime, timedelta
 import time
+import tweepy
 
-url = "https://api.opensea.io/api/v1/events"
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
 
-last_ts = (datetime.now() - timedelta(minutes=10)).timestamp()
 
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+OPENSEA_URL = "https://api.opensea.io/api/v1/events"
+
+#  Keep a backlog of 15 minutes in case of api outages 
+last_ts = (datetime.now() - timedelta(minutes=15)).timestamp()
+
+last_batch_ids = []
 while True:
   print("sales since: ", str(datetime.fromtimestamp(last_ts)))
   try:
@@ -14,12 +33,26 @@ while True:
 
     headers = {"Accept": "application/json"}
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request("GET", OPENSEA_URL, headers=headers, params=querystring)
 
-    for event in response.json()['asset_events']:
+    events = response.json()['asset_events']
+
+    for event in events:
+      if event['id'] in last_batch_ids:
+        continue
       price = round(int(event['total_price']) /  1000000000000000000, 4)
-      print(event['asset']['name'], ' sold for ', price, event['payment_token']['symbol'])
-    last_ts = datetime.now().timestamp()
+      msg = "{} was purchased for {} {} #THEDOGEPOUNDNFT {}".format(
+        event['asset']['name'],
+        price,
+        event['payment_token']['symbol'],
+        event['asset']['permalink'])
+      print(msg)
+      api.update_status(msg)
+
+
+    last_ts = (datetime.now() - timedelta(minutes=15)).timestamp()
+    last_batch_ids = [e['id'] for e in events]
+    print(last_batch_ids)
   except Exception as e:
     print(e)
   time.sleep(60)
